d174fbb2cafa81c0070dd1cbfd396e585eece290
rikky <son.levuthai@gmail.com>
2019-07-20 11:08:28 +0700

    remove messy test codes

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const {
     mountYargs
 }                       = require('./lib/utils');
 
+const HeadlessLocal     = require('./lib/Protocols/HeadlessLocal');
+const HttpOAuth2        = require('./lib/Protocols/HttpOAuth2');
+const HttpTokenBearer   = require('./lib/Protocols/HttpTokenBearer');
+
 exports.Gateway         = require('./lib/Gateway');
 exports.UnAuthenticated = require('./lib/UnAuthenticated');
 exports.Authenticator   = Authenticator;
@@ -18,3 +22,19 @@ exports.mountKoa        = mountKoa;
 exports.mountSocketIO   = mountSocketIO;
 exports.mountYargs      = mountYargs;
 
+// Basic protocols
+exports.HeadlessLocal   = HeadlessLocal;
+exports.HttpOAuth2      = HttpOAuth2;
+exports.HttpTokenBearer = HttpTokenBearer;
+
+// Derived protocols to transport frameworks
+exports.KoaLocal        = mountKoa()(HeadlessLocal);
+exports.ExpressLocal    = mountExpress()(HeadlessLocal);
+exports.SocketIOLocal   = mountSocketIO()(HeadlessLocal);
+exports.YargsLocal      = mountYargs()(HeadlessLocal);
+
+exports.KoaOAuth2       = mountKoa()(HttpOAuth2);
+exports.ExpressOAuth2   = mountExpress()(HttpOAuth2);
+
+exports.KoaToken        = mountKoa()(HttpTokenBearer);
+exports.ExpressToken    = mountExpress()(HttpTokenBearer);
