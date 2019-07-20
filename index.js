948ee033ca0bb9bf7a8f6f87185b557335cba656
rikky <son.levuthai@gmail.com>
2019-07-20 23:16:17 +0700

    session protocol

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
 const HeadlessLocal     = require('./lib/Protocols/HeadlessLocal');
 const HttpOAuth2        = require('./lib/Protocols/HttpOAuth2');
 const HttpTokenBearer   = require('./lib/Protocols/HttpTokenBearer');
+const HttpSession       = require('./lib/Protocols/HttpSession');
 
 exports.Gateway         = require('./lib/Gateway');
 exports.UnAuthenticated = require('./lib/UnAuthenticated');
@@ -24,16 +25,24 @@ exports.mountSocketIO   = mountSocketIO;
 exports.HeadlessLocal   = HeadlessLocal;
 exports.HttpOAuth2      = HttpOAuth2;
 exports.HttpTokenBearer = HttpTokenBearer;
+exports.HttpSession     = HttpSession;
 
 // Derived protocols to transport frameworks
+
+// Local
 exports.KoaLocal        = mountKoa()(HeadlessLocal);
 exports.ExpressLocal    = mountExpress()(HeadlessLocal);
 exports.SocketIOLocal   = mountSocketIO()(HeadlessLocal);
 
+// OAuth2
 exports.KoaOAuth2       = mountKoa()(HttpOAuth2);
 exports.ExpressOAuth2   = mountExpress()(HttpOAuth2);
 
+// Token
 exports.KoaToken        = mountKoa()(HttpTokenBearer);
 exports.ExpressToken    = mountExpress()(HttpTokenBearer);
-
 exports.SocketIOToken   = require('./lib/Protocols/SocketIOToken');
+
+// Session
+exports.KoaSession      = mountKoa()(HttpSession);
+exports.ExpressSession  = mountExpress()(HttpSession);
